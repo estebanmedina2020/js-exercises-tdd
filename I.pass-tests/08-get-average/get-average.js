eea31f90a3eb87264d29dff86c481809d1d69bd3
Esteban <emedina@creatonico.com>
2020-06-02 08:14:52 +0200

    finish exercise 08

--- I.pass-tests/08-get-average/get-average.js
+++ I.pass-tests/08-get-average/get-average.js
@@ -1,3 +1,12 @@
 // the input is an array of numbers and strings
 // return the average of all the numbers
 // be sure to exclude the strings
+
+function average(numbers) {
+  let num = numbers.filter((element) => typeof element === "number");
+  let sum = num.reduce((previous, current) => (current += previous));
+  let avg = sum / num.length;
+  return avg;
+}
+
+module.exports = average;
